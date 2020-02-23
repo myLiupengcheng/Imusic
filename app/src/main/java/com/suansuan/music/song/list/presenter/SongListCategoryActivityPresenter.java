06342fce207cade8c561ce09413f318cf224e06f
刘酸酸 <1065982915@qq.com>
2020-02-23 11:46:59 +0800

    clear SongListCategoryActivityPresenter

--- app/src/main/java/com/suansuan/music/song/list/presenter/SongListCategoryActivityPresenter.java
+++ app/src/main/java/com/suansuan/music/song/list/presenter/SongListCategoryActivityPresenter.java
@@ -18,16 +18,9 @@ import java.util.List;
 import okhttp3.Call;
 
 public class SongListCategoryActivityPresenter implements ActivityPresenter, MusicNetworkCallback {
-
-    private List<SongListCategoryGroup> songListCategoryGroupList;
-
-    private static final int MESSAGE_COMPLETE = 0x00001;
-
-    SongListCategoryActivityPresenterHandler mHandler;
-
     @Override
     public void onCreate() {
-        NetWorkManager.getInstance().requestGet(MusicConstant.URI_MUSIC_SONG_LIST_CATEGORIES, this);
+
     }
 
     @Override
@@ -37,28 +30,49 @@ public class SongListCategoryActivityPresenter implements ActivityPresenter, Mus
 
     @Override
     public void onSuccess(Call call, String response) {
-        if (!TextUtils.isDigitsOnly(response)) {
-            SongListCategoryParser songListCategoryParser = new SongListCategoryParser();
-            songListCategoryParser.parserJsonToBean(response);
-            songListCategoryGroupList = songListCategoryParser.getParserData();
-            mHandler.obtainMessage(MESSAGE_COMPLETE).sendToTarget();
-        }
+
     }
 
-    public static class SongListCategoryActivityPresenterHandler extends Handler {
-        private WeakReference<SongListCategoryActivityPresenter> mPresenterReference;
-        public SongListCategoryActivityPresenterHandler (WeakReference<SongListCategoryActivityPresenter> presenterReference) {
-            mPresenterReference = presenterReference;
-        }
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == MESSAGE_COMPLETE) {
-                SongListCategoryActivityPresenter songListActivityPresenter = mPresenterReference.get();
-                if (songListActivityPresenter.mCallback != null) {
-                    songListActivityPresenter.mCallback.loadCallbackComplete(songListActivityPresenter.songListCategoryGroupList);
-                    songListActivityPresenter.songListCategoryGroupList = null;
-                }
-            }
-        }
-    }
+//    private List<SongListCategoryGroup> songListCategoryGroupList;
+//
+//    private static final int MESSAGE_COMPLETE = 0x00001;
+//
+//    SongListCategoryActivityPresenterHandler mHandler;
+//
+//    @Override
+//    public void onCreate() {
+//        NetWorkManager.getInstance().requestGet(MusicConstant.URI_MUSIC_SONG_LIST_CATEGORIES, this);
+//    }
+//
+//    @Override
+//    public void onFailure(Call call, IOException e) {
+//
+//    }
+//
+//    @Override
+//    public void onSuccess(Call call, String response) {
+//        if (!TextUtils.isDigitsOnly(response)) {
+//            SongListCategoryParser songListCategoryParser = new SongListCategoryParser();
+//            songListCategoryParser.parserJsonToBean(response);
+//            songListCategoryGroupList = songListCategoryParser.getParserData();
+//            mHandler.obtainMessage(MESSAGE_COMPLETE).sendToTarget();
+//        }
+//    }
+//
+//    public static class SongListCategoryActivityPresenterHandler extends Handler {
+//        private WeakReference<SongListCategoryActivityPresenter> mPresenterReference;
+//        public SongListCategoryActivityPresenterHandler (WeakReference<SongListCategoryActivityPresenter> presenterReference) {
+//            mPresenterReference = presenterReference;
+//        }
+//        @Override
+//        public void handleMessage(Message msg) {
+//            if (msg.what == MESSAGE_COMPLETE) {
+//                SongListCategoryActivityPresenter songListActivityPresenter = mPresenterReference.get();
+//                if (songListActivityPresenter.mCallback != null) {
+//                    songListActivityPresenter.mCallback.loadCallbackComplete(songListActivityPresenter.songListCategoryGroupList);
+//                    songListActivityPresenter.songListCategoryGroupList = null;
+//                }
+//            }
+//        }
+//    }
 }
